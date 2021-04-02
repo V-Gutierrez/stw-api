80446947729b68feb73c4e2315e226b82c81e81c
Victor Gutierrez <victor.gutierrez@codeminer42.com>
2021-04-02 16:49:18 -0400

    Feat: implement Header initial version

---
+++ src/tests/components/Header/Header.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import { Header } from '../../../components/Header'
+
+import React from 'react'
+
+describe('Header Component', () => {
+  it('should render properly', () => {
+
+    render(<Header />)
+
+    const HeaderContainer = screen.getByTestId('header-container')
+
+    expect(HeaderContainer).toHaveTextContent('Star Wars API')
+    expect(HeaderContainer).toHaveClass('header-container')
+    })
+})
