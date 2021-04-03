8e6c8f47854f368092d322e884df9ed0e50e9600
Victor Gutierrez <victor.gutierrez@codeminer42.com>
2021-04-03 13:06:14 -0400

    Chore: change meta tag to _app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,6 @@
 import React from 'react'
 import { Homepage } from 'views/Homepage/Homepage'
-import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Star Wars API - by Victor Gutierrez</title>
-        <meta
-          http-equiv='Content-Security-Policy'
-          content='upgrade-insecure-requests'
-        />
-      </Head>
-      <Homepage />
-    </>
-  )
+  return <Homepage />
 }
