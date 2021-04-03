ac26c7cd12044f3447d8aa5860ec5deaa0412333
Victor Gutierrez <victor.gutierrez@codeminer42.com>
2021-04-02 22:49:12 -0400

    Fix: fix bugs in tests

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 - TODO : Search https://swapi.dev/api/people/?search=r2 using Search Context
 - TODO : e2e testing
+use some starwarsWiki
--- src/tests/components/PeopleList/Pager/Pager.test.tsx
+++ src/tests/components/PeopleList/Pager/Pager.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react'
-import Pager from '../../../../components/PeopleList/components/Pager/Pager'
+import { Pager } from '../../../../components/PeopleList/components/Pager/Pager'
 
 import React from 'react'
 
